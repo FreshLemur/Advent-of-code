deee6c952d140911abc20f504e759961ee72cfa9
FreshLemur <dimalimaredmi@gmail.com>
2024-12-17 11:50:26 +0000

    Resolved Day 2

--- 2024/Day 2/loadData.js
+++ 2024/Day 2/loadData.js
@@ -3,28 +3,45 @@ export default async function loadData() {
     const response = await fetch("./input.txt");
     const inputData = await response.text();
     const splittedInputData = inputData.split('\n');
-
     let amountOfSafeReports = 0;
 
     splittedInputData.map((item) => {
-      const splittedItemToNumber = item.split(" ").map(Number);
-
-      const isSafe = splittedItemToNumber.every((number, index) => {
-        const exmpl = splittedItemToNumber[index + 1] - number;
+      const example = item.split(" ").map(Number);
+      
+      const isSafe = example.every((number, index) => {
+        const report = example[index + 1] - number;
+        
+        if (report <= 0 || report > 3 || typeof report !== 'number') {
+          
+          example.reverse();
     
-        if (exmpl <= 0 || exmpl > 3 || typeof exmpl !== 'number') {
+          const isSafeReverse = example.every((number, index) => {
+            const report = example[index + 1] - number;
+    
+            if (report <= 0 || report > 3 || typeof report !== 'number') {
+              return;
+            } 
+
+            return true;
+          })
+    
+          if (isSafeReverse) {
+            return true;
+          }
+    
           return false; 
         }
-        
+    
         return true;
       })
+    
       if (isSafe) {
         amountOfSafeReports++;
       }
     })
 
+    // Right output is 246
     console.log(amountOfSafeReports);
-
   } catch (error) {
     console.error('Error loading the text file:', error);
   }
